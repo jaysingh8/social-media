e02fe6b83e8320fa581da1d5e37fb0f6dc65e773
Jay Singh <jaysinghrajput265@gmail.com>
2026-04-16 00:09:44 +0530

    your message

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://social-media-kappa-six.vercel.app/"],
     credentials: true,
   },
 });
